ae0b54176d871676614011e93730005506275103
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-04-02 11:53:41 +0100

    Async / Await with Fetch();

--- async-await.js
+++ async-await.js
@@ -39,3 +39,15 @@ async function init() {
 }
 
 init();
+
+// Async / Await with Fetch()
+
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    const data = await res.json();
+
+    console.log(data);
+}
+
+fetchUsers();
